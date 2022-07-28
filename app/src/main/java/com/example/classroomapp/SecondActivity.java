d11c2def31d90a1ae6bed3f14a2dc7be929fbb78
rosianeavila <64842310+rosianeavila@users.noreply.github.com>
2022-07-28 13:15:47 -0300

    Update SecondActivity.java
    primeira alteração.

--- app/src/main/java/com/example/classroomapp/SecondActivity.java
+++ app/src/main/java/com/example/classroomapp/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.classroomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -23,6 +26,52 @@ public class SecondActivity extends AppCompatActivity {
         cal.set(2022, 07, 27);
         int semanaAtual = cal.get(Calendar.WEEK_OF_MONTH);
         textSemana.setText(" Semana: " + Integer.toString(semanaAtual));
+
+        try {
+            SQLiteDatabase bancoDados = openOrCreateDatabase("classRoomApp", MODE_PRIVATE, null);
+
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS Professor(CPF Integer,\n" +
+                    "  nome Varchar(100),\n" +
+                    "  dataNascimento Date,\n" +
+                    "  PRIMARY KEY (CPF))");
+
+            bancoDados.execSQL("INSERT INTO Professor(CPF, nome, dataNascimento) VALUES (12312153726, 'Sabrina','01/01/1988')");
+            bancoDados.execSQL("INSERT INTO Professor(CPF, nome, dataNascimento) VALUES (55448384102, 'Ana','02/02/1988')");
+            bancoDados.execSQL("INSERT INTO Professor(CPF, nome, dataNascimento) VALUES (78591344383, 'Francine','03/03/1988')");
+
+            //bancoDados.execSQL("INSERT INTO CadastroAulas()"
+
+                    bancoDados.execSQL("CREATE TABLE IF NOT EXISTS CadastroAulas(codCadastroAulas Integer,\n" +
+                    "  CPFProfessor Integer,\n" +
+                    "  diaDaSemana Varchar(100),\n" +
+                    "  descAula Varchar(100),\n" +
+                    "  horarioAula Time,\n" +
+                    "  qtdMaximaAlunos Integer,\n" +
+                    "  PRIMARY KEY (codCadastroAulas),\n" +
+                    "  CONSTRAINT CPFProfessor FOREIGN KEY (CPFProfessor) REFERENCES Professor(CPF))");
+
+            bancoDados.execSQL("INSERT INTO CadastroAulas(codCadastroAulas, CPFProfessor, diaDaSemana, descAula, horarioAula, qtdMaximaAlunos)\n" +
+            " VALUES (1, 12312153726, 'Segunda', 'GENERAL ENGLISH', '18:00', 4)");
+
+            Cursor cursor = bancoDados.rawQuery("SELECT * FROM CadastroAulas ", null);
+
+            int indiceCodAula = cursor.getColumnIndex("codCadastroAulas");
+            int indiceCPFProfessor = cursor.getColumnIndex("CPFProfessor");
+            int indicediaDaSemanaa = cursor.getColumnIndex("diaDaSemana");
+            int indicedescAula = cursor.getColumnIndex("descAula");
+            int indicehorarioAula = cursor.getColumnIndex("horarioAula");
+            int indiceqtdMaximaAlunos = cursor.getColumnIndex("qtdMaximaAlunos");
+
+            cursor.moveToFirst();
+
+            while (cursor!=null){
+                Log.i("Resultado - Aula: ", cursor.getString(indicedescAula));
+
+                cursor.moveToNext();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
     }
